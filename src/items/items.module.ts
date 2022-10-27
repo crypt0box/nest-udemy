183503f616b7092fc75479821890f11672b0ebab
cryptobox <pray,happy76@gmail.com>
2022-10-27 21:49:18 +0900

    feat: implement db logic

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ItemRepository } from './item.repository';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ItemRepository])],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
